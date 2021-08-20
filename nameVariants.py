9ed1c9df84c778f757d0029ed9aec9b2dc7056d6
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2021-08-20 11:46:53 -0400

    Added West Arsi variant

--- nameVariants.py
+++ nameVariants.py
@@ -8,7 +8,7 @@ variantDict = {
     "South Wolo": ["Debub Wello", "South Wello"], "Maekelawi Gonder": ["Central Gonder"], "West Gonder": ["Mirab Gonder"], "North Shewa": ["Semen Shewa"], "Waghamera": ["Waghimra"],
     "West Gojam": ["Mirab Gojjam", "West Gojjam"],
     "Oromia Region": ["Oromiya Region"], "East Wellega": ["Misrak Wollega"], "West Shewa": ["Mirab Shewa"], "West Wellega": ["Mirab Wollega", "West Wollega"], "East Harerghe": ["Mierak Hararge", "East Hararge"],
-    "South West Shewa": ["Debub Mirab Shewa"], "West Harerghe": ["Mirab Hararghe", "West Hararghe"], "Debub Mirab Shewa": ["South West Shewa"], "Horoguduru Wellega": ["Horo Guduru Wollega"],
+    "South West Shewa": ["Debub Mirab Shewa"], "West Harerghe": ["Mirab Hararghe", "West Hararghe"], "Debub Mirab Shewa": ["South West Shewa"], "Horoguduru Wellega": ["Horo Guduru Wollega"], "West Arsi": ["West arsi"],
     "Mirab Guji": ["West Guji"], "Oromia": ["Oromiya", "Oromiya Liyu Zone", "Oromiya Special Zone"],
     "Benshangul-Gumuz": ["Benishangul Gumuz"], "Mao Komo": ["Mao Ena Komo", "Mao komo"], "Kemeshi": ["Kemashi"], "Asosa": ["Assosa"], "Pawe Special": ["Pawe special"],
     "S.N.N.P.R": ["SNNP"], "Yem Sp. Wereda": ["Yem Liyu", "Yem Special"], "Segen People": ["Segen Akababi Hizboch", "Segen Peoples'", "Segen Peoples’"], "Slite": ["Silte", "Silite"], "Konta Special": ["Konta Liyu", "Konta Sp. Wereda"],
